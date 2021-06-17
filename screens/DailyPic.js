dfbd5ad7a72052d98645a285a8f2593f8ebeff6e
Anirudh-030307 <65300694+Anirudh-030307@users.noreply.github.com>
2021-06-17 18:35:56 +0530

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class DailyPicScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>DAILY PIC SCREEN</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
